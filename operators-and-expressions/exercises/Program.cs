9d9409543812c2c5bce6184950ddecd259c5cd05
Wechuli <wechulipaul@yahoo.com>
2019-05-06 12:35:25 +0300

    exercises

---
+++ operators-and-expressions/exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
