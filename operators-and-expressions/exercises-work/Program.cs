30824e1e083b30811b5bcb04ba824a7ae38e52c8
wechuli <wechulipaul@yahoo.com>
2019-05-07 12:58:21 +0300

    porting application

---
+++ operators-and-expressions/exercises-work/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercises_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Write an expression that checks whether an integer is odd or even.
+            int number = 35;
+            string odd_even = (number % 2) == 0 ? "even" : "odd";
+            Console.WriteLine(odd_even);
+
+            //2. Write a Boolean expression that checks whether a given integer is divisible by both 5 and 7, without a remainder.
+            bool div5_7 = (number % 5 == 0) && (number % 7 == 0);
+            Console.WriteLine(div5_7);
+
+            //3. Write an expression that looks for a given integer if its third digit (right to left) is 7.
+            int longnumber = 857645;
+            bool third7 = (longnumber / 100) % 10 == 7;
+            Console.WriteLine(third7);
+        }
+    }
+}
