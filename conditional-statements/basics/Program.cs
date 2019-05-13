f73611a7547f6f355a9e92e08befaabb478f2b99
wechuli <wechulipaul@yahoo.com>
2019-05-13 16:05:31 +0300

    more conditionals

--- conditional-statements/basics/Program.cs
+++ conditional-statements/basics/Program.cs
@@ -29,7 +29,17 @@ namespace basics
             Console.WriteLine("true == false? " + (true == false)); // False
 
 
-            //In .NET Framework, there are reference data types that do not contain their value(unlike the value types), but contain address of the memory in the heap where thier value is located
+            //In .NET Framework, there are reference data types that do not contain their value(unlike the value types), but contain address of the memory in the heap where thier value is located.Strings, arrays and classes are such types. They behave like a pointer to some value and can have the value null i.e no value. When comparing reference type variables, we compare the addresses they hold i.e we check whether they point to the same location in the memory- the same object
+
+            string str = "beer";
+            string anotherStr = str;
+            Console.WriteLine(typeof(string));
+            Console.WriteLine(str == anotherStr);  //true
+            string athirdString = "beer";
+            Console.WriteLine(str == athirdString); //The == has a special behavior when comparing strings, but for the rest of the reference types(like arrays or classes) it applies comparion by address
+            //If we convert the vairbles to objects and then compare them, we will get a comparison of the addressess in the heap where their values are located
+            Console.WriteLine((object)str == (object)athirdString);
+
         }
     }
 }
