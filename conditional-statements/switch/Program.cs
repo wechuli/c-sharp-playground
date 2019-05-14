9b8bd17829839ac357039e8b05dc975160eec014
wechuli <wechulipaul@yahoo.com>
2019-05-14 13:23:25 +0300

    more switching

--- conditional-statements/switch/Program.cs
+++ conditional-statements/switch/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
-namespace switch
+namespace switching
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //The switch statement is a clear way to implement selection among many options - namely a choice among a few alternatives ways for executing the code. It requires a selector, which is calculated to a certain value. The selector type could be an integer number, char, string or enum. If we want to use for example an array or a float as a selector, it will not work
+
+            Console.Write("Please enter an integer number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 2:
+                    Console.WriteLine("Oh good, a prime number");
+                    break;
+                case 6:
+                    Console.WriteLine("How did you get the number?");
+                    break;
+                default:
+                    Console.WriteLine("Sorry buddy, no case for you");
+                    break;
+            }
+
+            Console.WriteLine("Please enter a book's name: ");
+            string book = Console.ReadLine();
+            switch (book)
+            {
+                case "Mid":
+                    Console.WriteLine("Best goddam book ever");
+                    break;
+                case "Pride":
+                    Console.WriteLine("Cute story, happy ending");
+                    break;
+                default:
+                    Console.WriteLine("You need to update your Library, that's gabbage");
+                    break;
+            }
         }
     }
 }
