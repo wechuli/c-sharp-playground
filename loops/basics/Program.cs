5c947f6c28bd2c1b374e238babb7200bdeb04816
wechuli <wechulipaul@yahoo.com>
2019-05-16 14:46:55 +0300

    while loop

---
+++ loops/basics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A loop is a basic programming construct that allows repeated execution of a fragment of source code
+            //The condition is any expression that returns a Boolean result - true or false
+
+
+            //While loop
+            int counter = 0;
+
+            while (counter <= 9)
+            {
+                //Print the counter value
+                Console.WriteLine($"Number: {counter}");
+                counter++;
+            }
+            //sum number from 1 to n
+            Console.Write("n= ");
+            int n = Int32.Parse(Console.ReadLine());
+            int num = 1;
+            int sum = 1;
+
+            Console.Write("The sum 1");
+            while (num < n)
+            {
+                num++;
+                sum += num;
+                Console.Write($"+ {num}");
+            }
+            Console.WriteLine("=" + sum);
+        }
+    }
+}
