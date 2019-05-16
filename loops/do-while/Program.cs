34241688e72df43825f1afcaf9a2285a3505dfad
Wechuli <wechulipaul@yahoo.com>
2019-05-16 23:41:58 +0300

    do while basics

--- loops/do-while/Program.cs
+++ loops/do-while/Program.cs
@@ -6,7 +6,8 @@ namespace do_while
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+       //The do-while loop is simila to the while loop, but it checks the condition after each of its loop body
+       //The do-while loop is used when we want to guarantee that the sequence of operations in it will be executed repeatedly and a
         }
     }
 }
