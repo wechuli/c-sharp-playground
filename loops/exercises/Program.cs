037355aaa9c5f5891d9f0979059bffbddd76f3c7
Wechuli <wechulipaul@yahoo.com>
2019-05-18 20:33:32 +0300

    exercises on loops

---
+++ loops/exercises/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Write a program that prints on the console the numbers from 1 to N. The number N should be read from the standard input.
+            Console.WriteLine("Please enter the number");
+            int n = Int32.Parse(Console.ReadLine());
+            // for (int i = 1; i <= n; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            //2.Write a program that prints on the console the numbers from 1 to N, which are not divisible by 3 and 7 simultaneously. The number N should be read from the standard input.
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+            //3.Write a program that reads from the console a series of integers and prints the smallest and largest of them.
+            Console.WriteLine("Please enter a series of integers and separate them with a space");
+            string numbers = Console.ReadLine();
+            string[] split = numbers.Split(" ");
+            int largest = int.MaxValue;
+            int smallest = int.MinValue;
+            foreach (var cha in split)
+            {
+                int number = Int32.Parse(cha);
+
+                if (number > smallest)
+                {
+                    smallest = number;
+                }
+                if (number < largest)
+                {
+                    largest = number;
+                }
+
+            }
+            Console.WriteLine($"The largest number is {largest} and the smallest is {smallest}");
+
+        }
+    }
+}
--- loops/nested_loops/Program.cs
+++ loops/nested_loops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace nested_loops
 {
@@ -6,7 +7,73 @@ namespace nested_loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Nested loops are programming constructs consisting of several loops located into each other. The innermost loop is executed more times, and the outermost - less times.
+
+            //After initialization of the first for loop, the execution of its body will start which contains the second (nested) loop. Its variable will be initialized, its condition will be checked and the code within its body will be executed, then the variable will be updated and the execution of thhe first for loop will continue, its variable will be updated and the whole second loop will be performed once again. The inner loop will be fully executed as many times as the body of the outer loop
+            Console.WriteLine("Please input n: ");
+
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{col} ");
+                }
+                Console.WriteLine();
+            }
+
+            //Prime Numbers in an Interval
+
+            Console.Write("n = ");
+            int no = int.Parse(Console.ReadLine());
+            Console.Write("m = ");
+            int m = int.Parse(Console.ReadLine());
+            for (int num = no; num <= m; num++)
+            {
+                bool prime = true;
+                int divider = 2;
+                int maxDivider = (int)Math.Sqrt(num);
+                while (divider <= maxDivider)
+                {
+                    if (num % divider == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    divider++;
+                }
+                if (prime)
+                {
+                    Console.Write(" " + num);
+                }
+            }
+
+            //Lottery 6/49 -Example
+
+            BigInteger counter = 0;
+            for (int i1 = 1; i1 <= 44; i1++)
+            {
+                for (int i2 = i1 + 1; i2 <= 45; i2++)
+                {
+                    for (int i3 = i2 + 1; i3 <= 46; i3++)
+                    {
+                        for (int i4 = i3 + 1; i4 <= 47; i4++)
+                        {
+                            for (int i5 = i4 + 1; i5 <= 48; i5++)
+                            {
+                                for (int i6 = i5 + 1; i6 <= 49; i6++)
+                                {
+                                    // Console.WriteLine(i1 + " " + i2 + " " +
+                                    // i3 + " " + i4 + " " + i5 + " " + i6);
+                                    counter += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"The number of combinations is {counter}");
         }
     }
 }
