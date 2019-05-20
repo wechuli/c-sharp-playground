092528c23fd3e3b31ebf85b1a98ed7ff2198f962
wechuli <wechulipaul@yahoo.com>
2019-05-20 14:01:38 +0300

    multidimensional arrays

---
+++ arrays/multidimensional/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multidimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
