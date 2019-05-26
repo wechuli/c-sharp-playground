5b2f714cff8d88b6be33a310c187d40dbe9be025
Wechuli <wechulipaul@yahoo.com>
2019-05-26 12:56:37 +0300

    exercise 5 on arrays

---
+++ arrays/exercises/question-5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.Write a program, which finds the maximal sequence of consecutively placed increasing integers. Example: {3, 2, 3, 4, 2, 2, 4} -> {2, 3, 4}.
+            int[] array = { 3, 2, 3, 4, 2, 4 };
+            int index = 0;
+            int tempCount = 1;
+            int count = 1;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    tempCount++;
+                }
+                if (array[i] > array[i + 1] || array[i] == array[i + 1])
+                {
+                    tempCount = 1;
+                }
+                if (tempCount > count)
+                {
+                    count = tempCount;
+                    index = i + 1;
+                }
+            }
+            for (int i = index + 1 - count; i < index + 1; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+
+        }
+    }
+}
---
+++ arrays/exercises/question1-4/exercises2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.Write a program, which finds the maximal sequence of consecutive equal elements in an array. E.g.: {1, 1, 2, 3, 2, 2, 2, 1} -> {2, 2, 2}.
+            int[] arr = { 1, 2, 3, 4, 4, 2, 1, 3, 2, 5, 2, 2, 2, 2, 2, 11, 11 };
+            int count = 1, tempCount = 1, number = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    tempCount++;
+                }
+                else
+                {
+                    tempCount = 1;
+                }
+
+                if (tempCount > count)
+                {
+                    count = tempCount;
+                    number = arr[i];
+                }
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("{0}, ", number);
+            }
+
+        }
+    }
+}
