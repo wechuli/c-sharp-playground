3f4584a954bf60ec5f777046a78275627b26f577
Wechuli <wechulipaul@yahoo.com>
2019-06-01 16:09:40 +0300

    number systems

---
+++ numerical-systems/basics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Numerical Systems are a way of representing numbers by a finite type-set of hraphical signs called digits
+            //The binary numerical system is the system which is used to represent and process numbers in modern computing machines. The main reason it is so widespread is explained with the fact that devices with two stable states are very simple to implement and the prooduction costs of binary arithmetic devices are very low
+            //the binary digits 0 and 1 can be easily represented in the computing machines as 'current' and 'no current' or as +5 and -5V
+
+            //The sequence of eight zeros or ones represents one byte, an ordinary eight bit binary number. All numkbers from 0 to 255 including can be represented in a sinle byte. In most cases this is not enough; as a result, several consecutive bytes can be used to represent a big number.
+        }
+    }
+}
