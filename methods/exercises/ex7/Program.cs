7f81ff480bf532e7ddab2c0ff635723c1be794af
Wechuli <wechulipaul@yahoo.com>
2019-06-12 15:30:12 +0300

    ex8

---
+++ methods/exercises/ex7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 2, 3, 3, 2, 2, 2, 1, 3, 4, 34, 3, 23, 2, 2, 2, 2, 2 };
+            int searchNumber = 2;
+            Console.WriteLine(NumberInArray(myArray, searchNumber));
+        }
+        public static int NumberInArray(int[] array, int number)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (number == array[i])
+                {
+                    count += 1;
+                }
+
+            }
+            return count;
+        }
+    }
+}
