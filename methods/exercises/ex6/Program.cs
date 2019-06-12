2f0e019eb54b06ab4f5e57645b69f60e0bbbc09d
Wechuli <wechulipaul@yahoo.com>
2019-06-12 15:23:34 +0300

    ex6

---
+++ methods/exercises/ex6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(returnLastLetter(1234432479));
+
+
+
+
+        }
+        public static string returnLastLetter(int numbers)
+        {
+            string[] numberNames = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string numToString = numbers.ToString();
+            int lastNumberIndex = Int32.Parse(numToString.Substring(numToString.Length - 1));
+            return numberNames[lastNumberIndex];
+
+        }
+
+    }
+}
