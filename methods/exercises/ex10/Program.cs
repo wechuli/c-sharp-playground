cecbb95c1ce4cdb6c1c7548ff8faa33f29810993
Wechuli <wechulipaul@yahoo.com>
2019-06-13 14:40:23 +0300

    ex10

---
+++ methods/exercises/ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a method that prints the digits of a given decimal number in a reversed order. For example 256, must be printed as 652.
+            DigitReverser(9566842);
+        }
+
+        public static void DigitReverser(int digits)
+        {
+            //turn the digits into a string
+            string sDigits = digits.ToString();
+            //initialize an empty string you will use to store the reversed string
+            string rDigits = "";
+            // loop through the sDigits string
+            for (int i = sDigits.Length - 1; i >= 0; i--)
+            {
+                rDigits += sDigits[i];
+            }
+            Console.WriteLine(rDigits);
+        }
+    }
+}
