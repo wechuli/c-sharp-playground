8baced781996668a5587c875aa87e9ab6e255e08
Wechuli <wechulipaul@yahoo.com>
2019-06-14 12:48:27 +0300

    factorial problem

---
+++ methods/exercises/ex12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that calculates and prints the n! for any n in the range [1…100].
+
+            Console.WriteLine(GetFactorial(50));
+
+        }
+        public static BigInteger GetFactorial(BigInteger number)
+        {
+            if (number < 1)
+            {
+                return 1;
+            }
+            return number * GetFactorial(number - 1);
+        }
+    }
+}
