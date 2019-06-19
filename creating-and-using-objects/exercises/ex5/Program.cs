7b1e3bb224ef4787ddf42654dbd85e37a1649453
Wechuli <wechulipaul@yahoo.com>
2019-06-19 14:33:27 +0300

    using math

--- creating-and-using-objects/exercises/ex5/Program.cs
+++ creating-and-using-objects/exercises/ex5/Program.cs
@@ -6,7 +6,20 @@ namespace ex5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Write a program which by given two sides finds the hypotenuse of a right triangle. Implement entering of the lengths of the sides from the standard input, and for the calculation of the hypotenuse use methods of the class Math.
+            Console.Write("Please enter the first side of the right angled triangle: ");
+            double right = Int64.Parse(Console.ReadLine());
+            Console.Write("Please enter the left side of the right angled triangle: ");
+            double left = Int64.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The hypotenuse is:{ReturnHypotenese(right, left)}");
+
+
+        }
+
+        static double ReturnHypotenese(double side1, double side2)
+        {
+            return Math.Round(Math.Sqrt((Math.Pow(side1, 2.0) + Math.Pow(side2, 2.0))), 4);
         }
     }
 }
