472394ba3a259a056d041a3ebac7f5ab20a2bf7f
Wechuli <wechulipaul@yahoo.com>
2019-06-19 12:54:20 +0300

    ex4

---
+++ creating-and-using-objects/exercises/ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int mil = Environment.TickCount;
+            Console.WriteLine($"Number of seconds: {mil / (1000.0)}");
+            Console.WriteLine($"Number of minutes: {mil / (1000.0 * 60)}");
+            Console.WriteLine($"Number of hours: {mil / (1000.0 * 60 * 60)}");
+            Console.WriteLine($"Number of days: {mil / (1000.0 * 60 * 60 * 24)}");
+
+
+        }
+    }
+}
