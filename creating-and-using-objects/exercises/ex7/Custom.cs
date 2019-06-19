1714843e78e053bf76a3ac4c6e43f9f6e6543135
Wechuli <wechulipaul@yahoo.com>
2019-06-19 23:59:20 +0300

    custom classes

---
+++ creating-and-using-objects/exercises/ex7/Custom.cs
@@ -0,0 +1,77 @@
+using System;
+namespace CreatingAndUsingObjects
+{
+    public class Sequence
+    {
+        // Static field, holding the current sequence value
+        private static int currentValue = 0;
+        // Intentionally deny instantiation of this class
+        private Sequence()
+        {
+        }
+        // Static method for taking the next sequence value
+        public static int NextValue()
+        {
+            currentValue++;
+            return currentValue;
+        }
+    }
+    public class Cat
+    {
+        // Field name
+        private string name;
+        // Field color
+        private string color;
+        public string Name
+        {
+            // Getter of the property "Name"
+            get
+            {
+                return this.name;
+            }
+            // Setter of the property "Name"
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Color
+        {
+            // Getter of the property "Color"
+            get
+            {
+                return this.color;
+            }
+            // Setter of the property "Color"
+            set
+            {
+                this.color = value;
+            }
+        }
+        // Default constructor
+        public Cat()
+        {
+            this.name = "Unnamed";
+            this.color = "gray";
+        }
+        // Constructor with parameters
+        public Cat(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+        // Method SayMiau
+        public void SayMiau()
+        {
+            Console.WriteLine("Cat {0} said: Miauuuuuu!", name);
+        }
+    }
+    namespace inner
+    {
+        public class caller
+        {
+            Cat newCat = new Cat();
+            
+        }
+    }
+}
