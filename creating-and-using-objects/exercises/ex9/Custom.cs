312fe4e1eebe1cd9e8d646aa868a27882a9e331e
Wechuli <wechulipaul@yahoo.com>
2019-06-21 13:52:01 +0300

    date

---
+++ creating-and-using-objects/exercises/ex9/Custom.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ex9
+{
+    public class DateCalculator
+    {
+
+        public DateCalculator()
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today);
+
+        }
+    }
+}
