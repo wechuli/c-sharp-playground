c2fa512d909b21dda733d092a6ee05daef3f52aa
Wechuli <wechulipaul@yahoo.com>
2019-07-07 15:28:56 +0300

    more exercises on strings

---
+++ strings-and-text-processing/exercises/ex13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that parses an URL in following format:
+            // [protocol]://[server]/[resource]
+            // It should extract from the URL the protocol, server and resource parts. For example, when http://www.cnn.com/video is passed, the result is:
+            // [protocol]="http"
+            // [server]="www.cnn.com"
+            // [resource]="/video"
+            string url = "http://www.ftpx.com/ftpintro.aspx";
+            string protocol = url.Substring(0, url.IndexOf("://"));
+            int complicatedNumber = url.IndexOf("/", (url.IndexOf("://") + 3)) - protocol.Length - 3;
+
+
+            string server = url.Substring(url.IndexOf("://") + 3, complicatedNumber);
+            string resource = url.Substring(server.Length + protocol.Length + 3);
+
+            Console.WriteLine($"Protocol: {protocol}");
+            Console.WriteLine($"Server: {server}");
+            Console.WriteLine($"Resource: {resource}");
+
+        }
+
+
+    }
+}
