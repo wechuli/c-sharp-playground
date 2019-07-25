f41f9feebc2db2f360721aa62b2aa699f46489ee
Wechuli <wechulipaul@yahoo.com>
2019-07-25 10:25:32 +0300

    fraction class

---
+++ defining-classes/exercises/ex6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fraction.Parse("3.3423ew"));
+        }
+    }
+
+    class Fraction
+    {
+        public double RationalNumber { get; set; }
+        public Fraction(string stringFraction)
+        {
+
+        }
+        public static double Parse(string stringFraction)
+        {
+            double parsedFraction;
+            
+            try
+            {
+                parsedFraction = Double.Parse(stringFraction);
+            }
+            catch (System.Exception e)
+            {
+
+                throw new Exception("Cannot parse the given string", e);
+            }
+            return parsedFraction;
+        }
+    }
+}
