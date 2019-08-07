297c500d69a02d17d37cfaba01dcda76bd6fe3f6
Wechuli <wechulipaul@yahoo.com>
2019-08-07 22:59:24 +0300

    ex7

---
+++ text-files/exercises/ex6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   Write a program that reads a list of names from a text file, arranges them in alphabetical order, and writes them to another file. The lines are written one per row.
+            string unSortedFile = "names.txt";
+            string sortedNames = "namesSorted.txt";
+            SortNamesAndWriteToNewFile(unSortedFile, sortedNames);
+
+        }
+
+        static void SortNamesAndWriteToNewFile(string namesfile, string sortedFile)
+        {
+
+            List<string> namesList = new List<string>();
+
+            try
+            {
+                StreamReader reader = new StreamReader(namesfile);
+                StreamWriter writer = new StreamWriter(sortedFile);
+
+                using (reader)
+                {
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        namesList.Add(line);
+                        line = reader.ReadLine();
+                    }
+                }
+                using (writer)
+                {
+                    namesList.Sort();
+                    foreach (var name in namesList)
+                    {
+                        writer.WriteLine(name);
+                    }
+                }
+
+                Console.WriteLine("Successfully sorted your names list");
+            }
+            catch (FileNotFoundException e)
+            {
+
+                Console.WriteLine("File not found");
+            }
+            catch (IOException io)
+            {
+                Console.WriteLine("A problem occured while opening the file");
+            }
+        }
+    }
+}
---
+++ text-files/exercises/test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<String> names = new List<String>();
+
+            names.Add("Paul");
+            names.Add("Wechuli");
+            names.Add("Brian");
+            names.Add("Nancy");
+
+
+            names.Sort();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
+
+
