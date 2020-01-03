8cf90dbb81342fd2094906b733f72e322d0ef1d3
Wechuli <wechulipaul@yahoo.com>
2020-01-03 22:02:15 +0300

    back to Lambda expressions

--- lambda-linq/concepts/lambda_intro/Program.cs
+++ lambda-linq/concepts/lambda_intro/Program.cs
@@ -18,6 +18,7 @@ namespace lambda_intro
 
             List<Dog> dogs = new List<Dog>() { new Dog { Name = "Rex", Age = 4 }, new Dog { Name = "Sean", Age = 0 }, new Dog { Name = "Stacy", Age = 3 } };
             var names = dogs.Select(x => x.Name);
+            
             foreach (var item in names)
             {
                 Console.WriteLine(item);
