350cd95b16fef7c62cd57f91ef0b43825c81b149
Wechuli <wechulipaul@yahoo.com>
2020-01-16 11:55:42 +0300

    challenges

---
+++ programming_challenges/challenge1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //We are given HTML file named Problem1.html. Write a program, which removes all HTML tags and retains only the text inside them. Output should be written into the file Problem1.txt.
+        }
+    }
+}
