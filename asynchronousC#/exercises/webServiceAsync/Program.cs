e23a474d908d10b2b66c8db0e43dea060a777c50
Wechuli <wechulipaul@yahoo.com>
2020-02-05 10:37:50 +0300

    using webclient async methods

---
+++ asynchronousC#/exercises/webServiceAsync/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace webServiceAsync
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos";
+            string downloadedString = await WebServicesClient.DownloadNetworkStringAsync(url);
+            Console.WriteLine(downloadedString);
+        }
+    }
+
+    public static class WebServicesClient
+    {
+
+        public static async Task<string> DownloadNetworkStringAsync(string url)
+        {
+            var uri = new Uri(url);
+            var webClient = new WebClient();
+            var result = await webClient.DownloadStringTaskAsync(uri);
+
+            return result;
+        }
+
+    }
+}
