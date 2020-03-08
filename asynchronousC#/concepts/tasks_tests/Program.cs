5e6714bf202ec6c0f325c2d8a587a595ec4391d2
Wechuli <wechulipaul@yahoo.com>
2020-03-08 10:03:09 +0300

    exe

---
+++ asynchronousC#/concepts/tasks_tests/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace tasks_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+            {
+                // Just Loop
+                int ctr = 0;
+                for (int i = 0; i < 1000000; i++)
+                {
+                    ctr++;
+                }
+                Console.WriteLine($"Finished {ctr} loop iterations");
+            });
+
+            t.Wait();
+
+
+
+            Console.WriteLine("I am after the task ");
+
+        }
+    }
+}
